c898cdfd8aa7ad99e3074be663bf10ea087b783f
bernym23 <80132172+bernym23@users.noreply.github.com>
2022-10-28 08:07:26 -0600

    Update Sat Copernicus.py

--- Sat Copernicus.py
+++ Sat Copernicus.py
@@ -1,3 +1,5 @@
+#Script para obtener imagenes del Sentinel 2 En Qgia utilizando Google Earth Engine.
+
 #llamado al plugin
 import ee
 
@@ -16,9 +18,8 @@ def maskS2clouds(image):
     qa = image.select('QA60')
 
     # del 0,1.  1 trata de limpiar las nubes.
-    cloudBitMask =8<< 10
+    cloudBitMask = 8 << 10
     cirrusBitMask = 11 << 11
-    #esto lo tiene que arreglar, forma de concatenar en phython#
 
     # las 2 en cero quiere decir que esta "despejado"
     mask = qa.bitwiseAnd(cloudBitMask).eq(0),
@@ -29,25 +30,32 @@ def maskS2clouds(image):
 
 
 #llamada a la coleccion, vamos a declarar la variable Sat.
+
+#Para imagenes anteriores al 2022 usar
 Sat = (ee.ImageCollection('COPERNICUS/S2_SR')
+       
+#Para imagenes del 2022 en adelante usar
+Sat = (ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
            
               # Aquí se cambian las fechas.
-              .filter(ee.Filter.date('2022-5-1', '2022-5-28'))
+              .filter(ee.Filter.date('2021-6-1', '2021-6-30'))
               # Poner el rango de nubosidad maxima
-              .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',40 ))
+              .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',65 ))
               # aplicar la mascara de nubes.
               .map(maskS2clouds)
              )
 
+ 
+
 #Aquí se cambian las bandas a visualizar
+
 #Bandas 4,3,2 son imagenes en RGB
 visParams1 = {
     'min' : 0,
     'max' : 0.3,
     'bands' : ['B4', 'B3', 'B2']}
 
-#Aquí se centra el mapa en Venecia, con un zoom de 12   
-Map.setCenter(-84.25272, 10.36041,12)
+
 
 # Aquí se agrega el mapa RGB al canvas
 Map.addLayer(Sat.mean(),visParams1,"RGB")
@@ -55,14 +63,16 @@ Map.addLayer(Sat.mean(),visParams1,"RGB")
 #estos son los parametros para agregar la imagen NIR
 visParams2 = {
     'min' : 0,
-    'max' : 0.3,
-    'bands' : ['B8', 'B3', 'B2']}
+    'max' : 0.5,
+    'bands' : ['B6', 'B3', 'B2']}
  
 #con esto agregamos el mapa NIR al Canvas 
 Map.addLayer(Sat.mean(),visParams2,"NIR") 
 
 
+
 #Vamos a calcular el NDVI
+
 #Aquí hago un llamado a la imagen y uso filtros que puse anteriormente, recordemos que 'sat' es la variable de la imagen satelital
 Tiempo1b = Sat.reduce(ee.Reducer.median());
 
@@ -73,10 +83,14 @@ NDVI1 = Tiempo1b.normalizedDifference (['B8_median', 'B4_median']);
 
 #Vamos a darle color
 visParams3 = {
-  'max' : 1.0,
   'min' : 0,
+  'max' : 1.0,
+  
   'palette' : ['CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901', '66A000', '529400', '3E8601',
     '207401', '056201', '004C00', '023B01', '012E01', '011D01', '011301']
 }
 #Agregamos al canvas como otro layer y le ponemos como nombre NDVI
 Map.addLayer (NDVI1,visParams3, 'NDVI');
+
+#Aquí se centra el mapa puedes poner la coordenada que quieras, con un zoom de 12   
+Map.setCenter(-84.25272, 10.36041,12)
