af1744f708dfb08c69704721361003449e4a274c
bernym23 <80132172+bernym23@users.noreply.github.com>
2022-11-28 14:49:43 -0600

    Create Sat Copernicus.js

---
+++ Sat Copernicus.js
@@ -0,0 +1,73 @@
+//Aplicar la funcion de la mascara de nube
+function maskS2clouds(image) {
+    //Seleccionamos la banda de mascara de nubes
+     var qa = image.select('QA60')
+
+    //los numeros mas bajos me dan mayor limpieza de nubosidad.
+    var cloudBitMask = 10 << 10
+    var cirrusBitMask = 11 << 11
+
+    //las 2 en cero quiere decir que esta "despejado"
+  var  mask = qa.bitwiseAnd(cloudBitMask).eq(0)
+  .and(qa.bitwiseAnd(cirrusBitMask).eq(0))
+    
+    //Aquí se devuelve el valor de las mascaras
+    return image.updateMask(mask).divide(10000)}
+
+//llamada a la coleccion, vamos a declarar la variable Sat.
+//var Sat = (ee.ImageCollection('COPERNICUS/S2_SR')
+
+//Para imagenes del 2022 en adelante usar (quitar el #)
+var Sat = (ee.ImageCollection('COPERNICUS/S2_SR_HARMONIZED')
+           
+              // Aquí se cambian las fechas.
+              .filter(ee.Filter.date('2022-1-1', '2022-11-27'))
+              //Poner el rango de nubosidad maxima
+              .filter(ee.Filter.lt('CLOUDY_PIXEL_PERCENTAGE',30 ))
+              //aplicar la mascara de nubes.
+              .map(maskS2clouds)
+              //Recortamos
+              .filterBounds(ee.Geometry.BBox(-87.391732,7.462142,-82.034863,11.541207))
+             )
+
+//Aquí se cambian las bandas a visualizar
+
+//Bandas 4,3,2 son imagenes en RGB
+var visParams1 = {
+    'min' : 0,
+    'max' : 0.3,
+    'bands' : ['B4', 'B3', 'B2']}
+
+//Aquí se agrega el mapa RGB al canvas
+Map.addLayer(Sat.mean(),visParams1,"RGB")
+
+//estos son los parametros para agregar la imagen NIR
+var visParams2 = {
+    'min' : 0,
+    'max' : 0.5,
+    'bands' : ['B6', 'B3', 'B2']}
+
+//con esto agregamos el mapa NIR al Canvas 
+Map.addLayer(Sat.mean(),visParams2,"NIR") 
+
+//Vamos a calcular el NDVI
+
+//Aquí hago un llamado a la imagen y uso filtros que puse anteriormente
+//recordemos que 'sat' es la variable de la imagen satelital
+var Tiempo1b = Sat.reduce(ee.Reducer.median());
+
+//Calculamos el NDVI
+var NDVI1 = Tiempo1b.normalizedDifference (['B8_median', 'B4_median']);
+
+//Vamos a darle color
+var visParams3 = {
+  'min' : 0,
+  'max' : 1.0,
+   'palette' : ['CE7E45', 'DF923D', 'F1B555', 'FCD163', '99B718', '74A901', '66A000', '529400', '3E8601',
+    '207401', '056201', '004C00', '023B01', '012E01', '011D01', '011301']}
+
+//Agregamos al canvas como otro layer y le ponemos como nombre NDVI
+Map.addLayer (NDVI1,visParams3, 'NDVI');
+
+//Aquí se centra el mapa con un zoom de 12   
+//Map.setCenter(-84.25272, 10.36041,12)
